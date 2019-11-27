e43ef3a2db8a42e45a41169d50df92fd4505a9d6
Vasiliy <sidorenkovn@list.ru>
2019-11-27 14:04:48 +0300

    +pages

---
+++ src/page118/B/Clock.java
@@ -0,0 +1,13 @@
+package page118.B;
+
+public class Clock {
+    String time;
+
+    void setTime (String t){
+        time =t;
+    }
+
+    String getTime (){ // исправил "void" на "String"
+        return time;
+    }
+}
